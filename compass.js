0bfd07362bb5e28d4f0c312138b72c14f69031cc
Angus Trau <angus.amtra5@gmail.com>
2016-06-14 17:06:05 +1000

    implement new auth mechanism

--- compass.js
+++ compass.js
@@ -1,51 +1,20 @@
-var Q = require("q");
-var request = require("browser-request");
+"use strict";
+const events = require("events");
+const Q = require("q");
+const request = require("request");
 
-var Auth = require("./auth.js");
-var Schedule = require("./schedule.js");
+const Auth = require("./auth.js");
+const Schedule = require("./schedule.js");
 
-function Compass(config) {
-	if (!config || !config.url || !config.username || !config.password) throw new Error("Invalid config");
+class Compass extends Auth(events.EventEmitter) {
+	constructor(config) {
+		super();
 
-	this.promise = Q.defer();
-
-	this.jar = request.jar();
-	this.request = request.defaults({
-		baseUrl: config.url,
-		jar: this.jar
-	});
-
-	this.auth = new Auth(this.request, config.url, config.username, config.password);
-	this.auth.promise
-	.then(this.refresh)
-	.then(this.promise.resolve)
-	.done()
-}
-
-Compass.prototype.refresh = function() {
-	var deferred = Q.defer();
-
-	this.getSchedule()
-	.then(deferred.resolve)
-	.done();
-
-	return deferred.promise;
-}
-
-Compass.prototype.getSchedule() {
-	var deferred = Q.defer();
-
-	//Get main page
-	this.request("/", function(error, response, body) {
-		if (!error && response.statusCode == 200) {
-			this.schedule = new Schedule(body);
-			deferred.resolve(this.schedule);
-		} else {
-			deferred.reject(error);
-		}
-	});
-
-	return deferred.promise;
+		if (!config || !config.url || typeof(config.url) != "string" || !config.username || !config.password) throw new Error("Invalid config");
+		this.config = config;
+		
+		this.auth = new Auth(this.config);
+	}
 }
 
 module.exports = Compass;
