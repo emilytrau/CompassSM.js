4acd6842809a7d4e97f74f379351528ea51ade76
Angus Trau <angus.amtra5@gmail.com>
2016-06-14 17:57:40 +1000

    Auto refresh session every hour by default

--- auth.js
+++ auth.js
@@ -1,5 +1,4 @@
 "use strict";
-const events = require("events");
 const Q = require("q");
 const request = require("request");
 const cheerio = require("cheerio");
@@ -8,26 +7,27 @@ const ClassBase = require("./base.js");
 
 //Create Auth Mix-in
 let Auth = ClassBase => class extends ClassBase {
-	getNewToken() {
+	getNewSession() {
 		let deferred = Q.defer();
 
-		this.request = request.defaults({
+		let cookieJar = request.jar();
+		let request = request.defaults({
 			baseUrl: this.config.url,
 			headers: {
 				"User-Agent": this.config.useragent || "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36" //Chrome 51
 			},
 			followAllRedirects: true,
-			jar: request.jar()
+			jar: cookieJar
 		})
 
-		this.request("/login.aspx", (error, response, body) => {
+		request("/login.aspx", (error, response, body) => {
 			if (!error && response.statusCode == 200) {
 				//Get unique session codes
 				let $ = cheerio.load(body);
 				let __VIEWSTATE = $("#__VIEWSTATE").attr("value");
 				let __VIEWSTATEGENERATOR = $("#__VIEWSTATEGENERATOR").attr("value");
 
-				this.request.post("/login.aspx", {
+				request.post("/login.aspx", {
 					form: {
 						__VIEWSTATE: __VIEWSTATE,
 						__VIEWSTATEGENERATOR: __VIEWSTATEGENERATOR,
@@ -37,12 +37,26 @@ let Auth = ClassBase => class extends ClassBase {
 						rememberMeChk: "on"
 					}
 				}, (error, response, body) => {
-					console.log(body);
-				})
+					if (!error && response.statusCode == 200) {
+						//Successfully created session cookies
+						this.request = request;
+						this.cookieJar = cookieJar;
+
+						deferred.resolve();
+					} else {
+						deferred.reject(error);
+					}
+				});
 			} else {
-				this.emit("compass_error", "Invalid url", error);
+				deferred.reject(error);
 			}
 		});
+
+		//Create a new session every hour by default
+		if (this.sessionTimeoutID) clearTimeout(this.sessionTimeoutID);
+		this.sessionTimeoutID = sessionTimeout((t) => {t.getNewSession.call(t)}, this.config.sessionTimeout || 3600000, this);
+
+		return deferred.promise;
 	}
 }
 
--- compass.js
+++ compass.js
@@ -1,10 +1,9 @@
 "use strict";
-const events = require("events");
 const Q = require("q");
-const request = require("request");
 
-const ClassBase = require("./base.js")
+const ClassBase = require("./base.js");
 const Auth = require("./auth.js");
+const News = require("./news.js");
 const Schedule = require("./schedule.js");
 
 //Compass Class
@@ -16,7 +15,14 @@ class Compass extends Auth(ClassBase) {
 		if (!config || !config.url || typeof(config.url) != "string" || !config.username || !config.password) throw new Error("Invalid config");
 		this.config = config;
 
-		this.getNewToken();
+		this.getNewSession()
+		.then(() => {
+			//Logged in
+			//Load news and pass it a wrapper function to get the current request object as well as the config
+			this.news = new News(() => {return this.request;});
+			return this.news.getNews();
+		})
+		.done();
 	}
 }
 
