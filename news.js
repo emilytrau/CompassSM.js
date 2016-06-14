3b8a35eb44dc598473b9f155814d6adf3dec21e4
Angus Trau <angus.amtra5@gmail.com>
2016-06-14 21:25:04 +1000

    Added a way to download attatchments

--- news.js
+++ news.js
@@ -5,12 +5,37 @@ const moment = require("moment");
 
 const ClassBase = require("./base.js");
 
+class Attachment {
+	constructor(data, request) {
+		this.name = data.name;
+		this.isImage = data.IsImage;
+		this.request = request;
+
+		if (this.isImage) {
+			this.url = "/ImageDownload/1/" + data.AssetId;
+		} else {
+			this.url = "/Services/FileDownload/FileRequestHandler?FileDownloadType=1&file=" + data.AssetId;
+		}
+	}
+
+	download(stream) {
+		this.request()(this.url).pipe(stream);
+	}
+}
+
 class NewsItem {
-	constructor(data) {
+	constructor(data, request) {
 		this.title = data.Title;
 		this.content = cheerio("<div>" + data.Content1 + "</div>").text();
 		this.author = data.UserName;
 		this.postTime = moment(data.PostDateTime, "DD\/MM\/YYYY - hh:mm a").clone().toDate();
+
+		this.attachments = [];
+		if (data.Attachments.length > 0) {
+			for (var i=1; i<data.Attachments.length; i++) {
+				this.attachments.push(new Attachment(data.Attachments[i], request));
+			}
+		}
 	}
 }
 
@@ -36,7 +61,7 @@ class News extends ClassBase {
 			let data = body.d.data;
 			this.data = [];
 			for (var i=0; i<data.length; i++) {
-				this.data[i] = new NewsItem(data[i]);
+				this.data[i] = new NewsItem(data[i], this.request);
 			}
 
 			deferred.resolve(this.data);
