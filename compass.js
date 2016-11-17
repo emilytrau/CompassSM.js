5abc12bc488b40eb67ec48d6aa558e5a29496acd
Angus Trau <angus.amtra5@gmail.com>
2016-11-17 11:22:57 +1100

    Add repl for testing

--- compass.js
+++ compass.js
@@ -2,7 +2,15 @@ let Auth = require("./auth");
 
 module.exports = class Compass {
 	constructor(serverurl, username, password) {
-		this.url = serverurl;
-		this.auth = new Auth(username, password);
+		this.auth = new Auth(serverurl, username, password);
+	}
+
+	async initialise() {
+		await this.auth.initialise();
 	}
 }
+
+const repl = require("repl");
+C = module.exports;
+let r = repl.start("> ");
+r.context.C = C;
